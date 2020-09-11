b74acb50cd606112979a485890faf9a054a80242
YusufRamdhani <70943943+YusufRamdhani@users.noreply.github.com>
2020-09-11 08:03:33 +0700

    Add files via upload

---
+++ YusufRamdhani_XIIRplC_UTSPBO.java
@@ -0,0 +1,15 @@
+package utsucup;
+
+public class Setering {
+
+  public static void main(String[] args) {
+    String ch = "ABCDEFGHIJKLMNOPQRSTUVWXYZ=-";
+    char[] cc = {
+ch.charAt(24),ch.charAt(20),ch.charAt(18),ch.charAt(20),ch.charAt(5),ch.charAt(27),ch.charAt(17),ch.charAt(0),ch.charAt(12),ch.charAt(3),ch.charAt(7),ch.charAt(0),ch.charAt(13),ch.charAt(8)
+};
+    
+    for(int i=0; i<cc.length; i++){
+      System.out.print(cc[i]);
+    }
+  }
+}
